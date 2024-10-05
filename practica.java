c4586d2ac484f2a0efdedd46521477fc4609e1b2
laura <velasquezcoronell@gmail.com>
2024-10-05 10:27:27 -0500

    elimine edad

--- practica.java
+++ practica.java
@@ -4,8 +4,7 @@ public class practica {
         Scanner scanner = new Scanner(System.in);
         System.out.print("ingrese su nombre");
         String nombre = scanner.nextLine();
-        System.out.print("ingrese su edad");
-        int edad = scanner.nextInt();
+        
         System.out.print("ingrese su estatura");
         float estatura = scanner.nextFloat();
         System.out.println("hola, " + nombre + " tu edad es " + edad + " años y mides "+ estatura + " metros");
